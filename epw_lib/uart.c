f8f729457f1c29c97b1e2e398e225c4aa3032bc6
skylinen4s <skyline_n4s@hotmail.com>
2015-08-04 17:42:33 +0800

    fix the bug that cant print '0' in int

--- epw_lib/uart.c
+++ epw_lib/uart.c
@@ -188,6 +188,9 @@ void USART_putd(USART_TypeDef* USARTx, uint32_t number)
 		tmp_num[cnt] = temp+'0';
 		cnt++;
 	}
+	if(number == 0){
+		tmp_num[cnt++] = '0';
+	}
 	int j = 0;
 	while(cnt){
 		num[j++] = tmp_num[--cnt];
