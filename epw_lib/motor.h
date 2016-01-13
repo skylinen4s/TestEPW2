94bd50d2bd04faee10016fb964fcf84fa6a8d890
skylinen4s <skyline_n4s@hotmail.com>
2016-01-13 22:00:03 +0800

    add the pins for motor driver

--- epw_lib/motor.h
+++ epw_lib/motor.h
@@ -4,6 +4,19 @@
 
 /* Define all pins of the motor of EPW2 */
 #define MOTOR_PWM_PORT				GPIOD
+
+/* The switch of the motor drivers */
+#define MOTOR_RELAY_PIN				GPIO_Pin_9
+
+/* The switch to toggle constant voltage and control signal */
+#define MOTOR_LEFT_SW_PIN			GPIO_Pin_10
+#define MOTOR_RIGHT_SW_PIN			GPIO_Pin_11 
+
+/* Constant Voltage for motor driver initialization */
+#define MOTOR_LEFT_CV_PIN			GPIO_Pin_12 //Green  TIM4_CH1
+#define MOTOR_RIGHT_CV_PIN			GPIO_Pin_13 //Orange TIM4_CH2
+
+/* Control Signal */
 #define MOTOR_LEFT_PWM_PIN			GPIO_Pin_14 //Red  TIM4_CH3
 #define MOTOR_RIGHT_PWM_PIN			GPIO_Pin_15 //Blue TIM4_CH4
 
