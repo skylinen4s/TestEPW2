afa79adb9b1b1a740cda496479439269ebcfc245
skylinen4s <skyline_n4s@hotmail.com>
2016-03-23 16:03:37 +0800

    update .h file of record

--- epw_control/record.h
+++ epw_control/record.h
@@ -10,6 +10,8 @@
 
 void start_record(void);
 void close_record(void);
+void writeControlValue(uint32_t pwm_L, uint32_t pwm_R);
+uint32_t readControlValue(void);
 void recControlData(uint32_t pwm_L, uint32_t pwm_R, int enc_L, int enc_R);
 void record(void);
 void pwmrecord(void);
