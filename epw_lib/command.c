9c814551ad60402824639e21faaa6f537aaae6a8
skylinen4s <skyline_n4s@hotmail.com>
2016-03-24 15:13:13 +0800

    arrange code of command.c

--- epw_lib/command.c
+++ epw_lib/command.c
@@ -16,88 +16,82 @@ uint32_t inc = 1;
 struct receive_cmd_list * receive_cmd_type;
 
 void receive_task(){
-	//int i, j;
-	//while(1){
-		if(Receive_String_Ready){
-			if(received_string[0] == '+'){
-				SpeedValue_left += inc;
-				SpeedValue_right += inc;
-				mMove(SpeedValue_left,SpeedValue_right);
-				USART_puts(USART3, "left:");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right:");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
-			}else if(received_string[0] == '-'){
-				SpeedValue_left -= inc;
-				SpeedValue_right -= inc;
-				mMove(SpeedValue_left,SpeedValue_right);
-				USART_puts(USART3, "left:");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right:");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
-			}else if(received_string[0] == 'e'){
-				getEncoder();
-			}else if(received_string[0] == 'u'){
-				if(received_string[1] == 'a'){
-				    USART_puts(USART3, "Actu_A_up");
-				    set_linearActuator_A_cmd(LINEAR_ACTU_CW);
-				    USART_puts(USART3, "\r\n");
-				}else if(received_string[1] == 'b'){
-				    USART_puts(USART3, "Actu_B_up");
-				    set_linearActuator_B_cmd(LINEAR_ACTU_CW);
-				    USART_puts(USART3, "\r\n");
-				}
-			}else if(received_string[0] == 'd'){
-				if(received_string[1] == 'a'){
-				    USART_puts(USART3, "Actu_A_down");
-				    set_linearActuator_A_cmd(LINEAR_ACTU_CCW);
-				    USART_puts(USART3, "\r\n");
-				}else if(received_string[1] == 'b'){
-				    USART_puts(USART3, "Actu_B_down");
-				    set_linearActuator_B_cmd(LINEAR_ACTU_CCW);
-				    USART_puts(USART3, "\r\n");
-				}
-			}else if(received_string[0] == 'a'){
-				mPowerON();
-			}else if(received_string[0] == 'A'){
-				mPowerOFF();
-			}else if(received_string[0] == 'b'){
-				mSwitchON();
-			}else if(received_string[0] == 'B'){
-				mSwitchOFF();
-			}else if(received_string[0] == 'c'){
-				getCurData();
-
-			}else if(received_string[0] == 'f'){
-				test_forward();
-				USART_puts(USART3, "left:");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right:");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
-			}else if(received_string[0] == 't'){
-				check();
-			}else{
-				USART_puts(USART3, received_string);
-				USART_puts(USART3, "\r\n");
-			}
-			//}
-			/*Receive_String_Ready = 0;
-			for( i = 0 ; i< MAX_STRLEN ; i++){
-				received_string[i]= 0;*/
+	if(Receive_String_Ready){
+		if(received_string[0] == '+'){
+			SpeedValue_left += inc;
+			SpeedValue_right += inc;
+			mMove(SpeedValue_left,SpeedValue_right);
+			USART_puts(USART3, "left:");
+			USART_putd(USART3, SpeedValue_left);
+			USART_puts(USART3, " right:");
+			USART_putd(USART3, SpeedValue_right);
+			USART_puts(USART3, "\r\n");
 		}
-	//}
-}
-/*
-void parseString(unsigned char *received_string){
-	char *word = strtok(received_string,' ');
-	receive_cmd_type->Identifier[0] = word;
-	if (word = 'c'){
-			word = strtok(NULL,' ');
-			//receive_cmd_type->DIR_cmd = word;
-			//word = strtok(NULL,' ');
-			//receive_cmd_type->pwm_value = word;
+
+		else if(received_string[0] == '-'){
+			SpeedValue_left -= inc;
+			SpeedValue_right -= inc;
+			mMove(SpeedValue_left,SpeedValue_right);
+			USART_puts(USART3, "left:");
+			USART_putd(USART3, SpeedValue_left);
+			USART_puts(USART3, " right:");
+			USART_putd(USART3, SpeedValue_right);
+			USART_puts(USART3, "\r\n");
+		}
+
+		else if(received_string[0] == 'e'){
+			getEncoder();
+		}
+
+		else if(received_string[0] == 'u'){
+			if(received_string[1] == 'a'){
+			    USART_puts(USART3, "Actu_A_up");
+			    set_linearActuator_A_cmd(LINEAR_ACTU_CW);
+			    USART_puts(USART3, "\r\n");
+			}else if(received_string[1] == 'b'){
+			    USART_puts(USART3, "Actu_B_up");
+			    set_linearActuator_B_cmd(LINEAR_ACTU_CW);
+			    USART_puts(USART3, "\r\n");
+			}
+		}
+		else if(received_string[0] == 'd'){
+			if(received_string[1] == 'a'){
+			    USART_puts(USART3, "Actu_A_down");
+			    set_linearActuator_A_cmd(LINEAR_ACTU_CCW);
+			    USART_puts(USART3, "\r\n");
+			}else if(received_string[1] == 'b'){
+			    USART_puts(USART3, "Actu_B_down");
+			    set_linearActuator_B_cmd(LINEAR_ACTU_CCW);
+			    USART_puts(USART3, "\r\n");
+			}
+		}
+
+		else if(received_string[0] == 'a'){
+			mPowerON();
+		}
+		else if(received_string[0] == 'A'){
+			mPowerOFF();
+		}
+		else if(received_string[0] == 'b'){
+			mSwitchON();
+		}
+		else if(received_string[0] == 'B'){
+			mSwitchOFF();
+		}
+		else if(received_string[0] == 'c'){
+			getCurData();
+
+		}
+		else if(received_string[0] == 'f'){
+			test_forward();
+		}
+		else if(received_string[0] == 't'){
+			check();
+		}
+
+		else{
+			USART_puts(USART3, received_string);
+			USART_puts(USART3, "\r\n");
+		}
 	}
-}*/
+}
