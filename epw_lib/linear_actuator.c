8d584872d7d308a9f050bfac35ed004edc237582
skylinen4s <skyline_n4s@hotmail.com>
2016-05-04 15:48:58 +0800

    change indentation 2 to 4

--- epw_lib/linear_actuator.c
+++ epw_lib/linear_actuator.c
@@ -7,15 +7,14 @@
 typedef enum{
     ACTUATOR_GROUP_A,
     ACTUATOR_GROUP_B
-    }
-    actuator_group;
+}actuator_group;
 
 typedef enum{
-		ACTUATOR_STATE_IDLE,
-		ACTUATOR_STATE_MOVE_CW,                               
-		ACTUATOR_STATE_MOVE_CCW
-                }
-                actuator_state_t;
+    ACTUATOR_STATE_IDLE,
+    ACTUATOR_STATE_MOVE_CW,
+    ACTUATOR_STATE_MOVE_CCW
+}actuator_state_t;
+
 static actuator_state_t actuator_state_A, actuator_state_B;
 static actuator_A_cnt = 0, actuator_B_cnt = 0; 
 /* limit moving time of linear actuator.
@@ -28,158 +27,173 @@ xTimerHandle detect_LS_TimerB;
 
 #define DETECT_LS_POLLING_PERIOD  50//unit : ms
 
- 
+
 static void init_LS(void)
 {
-        GPIO_InitTypeDef GPIO_InitStruct;
-        /* Enable GPIO D clock. */
-        RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStruct;
+    /* Enable GPIO D clock. */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-        GPIO_InitStruct.GPIO_Pin =  LS_A_UPPER_PIN | LS_A_LOWER_PIN | LS_B_UPPER_PIN | LS_B_LOWER_PIN; 
+    GPIO_InitStruct.GPIO_Pin =  LS_A_UPPER_PIN | LS_A_LOWER_PIN | LS_B_UPPER_PIN | LS_B_LOWER_PIN;
 
-        GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
-        GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-        GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
-        GPIO_Init( LS_READ_PORT, &GPIO_InitStruct ); 
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_Init( LS_READ_PORT, &GPIO_InitStruct );
 }
 
 
 /* initialize the control pin(CP) for the linear actuator */
-static void init_CP(){
-		GPIO_InitTypeDef GPIO_InitStruct;
-		/* Enable GPIO E clock. */
-		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
-		GPIO_InitStruct.GPIO_Pin =  ACTU_A_IN1_PIN| ACTU_A_IN2_PIN | ACTU_B_IN3_PIN | ACTU_B_IN4_PIN ;
-	
-		GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;            // Alt Function - Push Pull
-		GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-		GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-		GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-		GPIO_Init( ACTU_CONTROL_PORT, &GPIO_InitStruct ); 
-		
-		GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN1_PIN,Bit_RESET);
-		GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN2_PIN,Bit_RESET);
-		GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN3_PIN,Bit_RESET);
-		GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN4_PIN,Bit_RESET);	
+static void init_CP()
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+    /* Enable GPIO E clock. */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+    GPIO_InitStruct.GPIO_Pin =  ACTU_A_IN1_PIN| ACTU_A_IN2_PIN | ACTU_B_IN3_PIN | ACTU_B_IN4_PIN ;
+
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;            // Alt Function - Push Pull
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init( ACTU_CONTROL_PORT, &GPIO_InitStruct );
+
+    GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN1_PIN,Bit_RESET);
+    GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN2_PIN,Bit_RESET);
+    GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN3_PIN,Bit_RESET);
+    GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN4_PIN,Bit_RESET);
 }
 
-void init_linear_actuator(){
-		init_CP();
-		init_LS();
+void init_linear_actuator()
+{
+    init_CP();
+    init_LS();
 
-    detect_LS_TimerA=xTimerCreate("Detect limit Switch state Polling",( DETECT_LS_POLLING_PERIOD), pdTRUE, ( void * ) 3,  detect_LS_A);
-    detect_LS_TimerB=xTimerCreate("Detect limit Switch state Polling",( DETECT_LS_POLLING_PERIOD), pdTRUE, ( void * ) 3,  detect_LS_B);
+    detect_LS_TimerA = xTimerCreate("Detect limit Switch A state Polling",(DETECT_LS_POLLING_PERIOD), pdTRUE, ( void * ) 3,  detect_LS_A);
+    detect_LS_TimerB = xTimerCreate("Detect limit Switch B state Polling",(DETECT_LS_POLLING_PERIOD), pdTRUE, ( void * ) 3,  detect_LS_B);
 }
 
-void set_linearActuator_A_cmd(int flag){
-     xTimerStart( detect_LS_TimerA, 0 );
-     if(flag==LINEAR_ACTU_STOP){
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN1_PIN,Bit_RESET);/* 0 */
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN2_PIN,Bit_RESET);/* 0 */
-         actuator_state_A = ACTUATOR_STATE_IDLE;
-     }/* STOP */
-     else if(flag==LINEAR_ACTU_CW && get_Linear_Actuator_A_LS_State()!=0x01 && get_Linear_Actuator_A_LS_State()!=0x03){ /*if not upper limited.*/
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN1_PIN,Bit_SET);/* 1 */
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN2_PIN,Bit_RESET);/* 0 */
-         actuator_state_A = ACTUATOR_STATE_MOVE_CW;
-     }/* FORWARD */
-     else if(flag==LINEAR_ACTU_CCW && get_Linear_Actuator_A_LS_State()!=0x02 && get_Linear_Actuator_A_LS_State()!=0x03){ /*if not lower limited*/
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN1_PIN,Bit_RESET);/* 0 */
-	 GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN2_PIN,Bit_SET);/* 1 */
-         actuator_state_A = ACTUATOR_STATE_MOVE_CCW;
-     }/* BACKWARD */
+void set_linearActuator_A_cmd(int flag)
+{
+    xTimerStart(detect_LS_TimerA, 0);
+
+    if(flag==LINEAR_ACTU_STOP){
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN1_PIN,Bit_RESET);/* 0 */
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN2_PIN,Bit_RESET);/* 0 */
+        actuator_state_A = ACTUATOR_STATE_IDLE;
+    }/* STOP */
+    else if(flag==LINEAR_ACTU_CW && get_Linear_Actuator_A_LS_State()!=0x01 && get_Linear_Actuator_A_LS_State()!=0x03){ /*if not upper limited.*/
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN1_PIN,Bit_SET);/* 1 */
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN2_PIN,Bit_RESET);/* 0 */
+        actuator_state_A = ACTUATOR_STATE_MOVE_CW;
+    }/* FORWARD */
+    else if(flag==LINEAR_ACTU_CCW && get_Linear_Actuator_A_LS_State()!=0x02 && get_Linear_Actuator_A_LS_State()!=0x03){ /*if not lower limited*/
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN1_PIN,Bit_RESET);/* 0 */
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_A_IN2_PIN,Bit_SET);/* 1 */
+        actuator_state_A = ACTUATOR_STATE_MOVE_CCW;
+    }/* BACKWARD */
 }
 
-void set_linearActuator_B_cmd(int flag){
-     xTimerStart( detect_LS_TimerB, 0 );
-     if(flag==LINEAR_ACTU_STOP){
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN3_PIN,Bit_RESET);/* 0 */
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN4_PIN,Bit_RESET);/* 0 */
-         actuator_state_B = ACTUATOR_STATE_IDLE;
-     }/* STOP */
-     else if(flag==LINEAR_ACTU_CW && get_Linear_Actuator_B_LS_State()!=0x01 && get_Linear_Actuator_B_LS_State()!=0x03){ /*if not upper limited.*/
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN3_PIN,Bit_SET);/* 1 */
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN4_PIN,Bit_RESET);/* 0 */
-         actuator_state_B = ACTUATOR_STATE_MOVE_CW;
-     }/* FORWARD */
-     else if(flag==LINEAR_ACTU_CCW && get_Linear_Actuator_B_LS_State()!=0x02 && get_Linear_Actuator_B_LS_State()!=0x03){ /*if not lower limited*/
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN3_PIN,Bit_RESET);/* 0 */
-         GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN4_PIN,Bit_SET);/* 1 */
-         actuator_state_B = ACTUATOR_STATE_MOVE_CCW;
-     }/* BACKWARD */
+void set_linearActuator_B_cmd(int flag)
+{
+    xTimerStart(detect_LS_TimerB, 0);
+
+    if(flag==LINEAR_ACTU_STOP){
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN3_PIN,Bit_RESET);/* 0 */
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN4_PIN,Bit_RESET);/* 0 */
+        actuator_state_B = ACTUATOR_STATE_IDLE;
+    }/* STOP */
+    else if(flag==LINEAR_ACTU_CW && get_Linear_Actuator_B_LS_State()!=0x01 && get_Linear_Actuator_B_LS_State()!=0x03){ /*if not upper limited.*/
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN3_PIN,Bit_SET);/* 1 */
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN4_PIN,Bit_RESET);/* 0 */
+        actuator_state_B = ACTUATOR_STATE_MOVE_CW;
+    }/* FORWARD */
+    else if(flag==LINEAR_ACTU_CCW && get_Linear_Actuator_B_LS_State()!=0x02 && get_Linear_Actuator_B_LS_State()!=0x03){ /*if not lower limited*/
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN3_PIN,Bit_RESET);/* 0 */
+        GPIO_WriteBit(ACTU_CONTROL_PORT,ACTU_B_IN4_PIN,Bit_SET);/* 1 */
+        actuator_state_B = ACTUATOR_STATE_MOVE_CCW;
+    }/* BACKWARD */
 }
 
-static void detect_LS_A(){
+static void detect_LS_A()
+{
     /*detect actuator A LS*/
     switch(actuator_state_A){
         case ACTUATOR_STATE_IDLE:
-           xTimerStop(detect_LS_TimerA, 0);
-           break;//wait the next state into.
+            xTimerStop(detect_LS_TimerA, 0);
+            break;//wait the next state into.
+
         case ACTUATOR_STATE_MOVE_CW:
-           actuator_A_cnt ++;
-           if(get_Linear_Actuator_A_LS_State()==0x01 || get_Linear_Actuator_A_LS_State()==0x03){
-               set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
-               actuator_state_A=ACTUATOR_STATE_IDLE;
-           }
-           if(actuator_A_cnt >= 20){
-               set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
-               actuator_state_A=ACTUATOR_STATE_IDLE;
-               actuator_A_cnt = 0;
-           }
-           break;
+            actuator_A_cnt ++;
+            if(get_Linear_Actuator_A_LS_State()==0x01 || get_Linear_Actuator_A_LS_State()==0x03){
+                set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
+                actuator_state_A = ACTUATOR_STATE_IDLE;
+            }
+            if(actuator_A_cnt >= 20){
+                set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
+                actuator_state_A = ACTUATOR_STATE_IDLE;
+                actuator_A_cnt = 0;
+            }
+            break;
+
         case ACTUATOR_STATE_MOVE_CCW:
             actuator_A_cnt ++;
             if(get_Linear_Actuator_A_LS_State()==0x02 || get_Linear_Actuator_A_LS_State()==0x03){
-               set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
-               actuator_state_A=ACTUATOR_STATE_IDLE;
+                set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
+                actuator_state_A = ACTUATOR_STATE_IDLE;
             }
             if(actuator_A_cnt >= 20){
-               set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
-               actuator_state_A=ACTUATOR_STATE_IDLE;
-               actuator_A_cnt = 0;
+                set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
+                actuator_state_A = ACTUATOR_STATE_IDLE;
+                actuator_A_cnt = 0;
             }
-           break;
+            break;
+
         default:
-           break;
+            break;
     }
 }
 
-static void detect_LS_B(){
+static void detect_LS_B()
+{
     /*detect actuator B LS*/
     switch(actuator_state_B){
-       case ACTUATOR_STATE_IDLE:
-           xTimerStop(detect_LS_TimerB, 0);
-           break;//wait the next command into.
-       case ACTUATOR_STATE_MOVE_CW:
-           actuator_B_cnt ++;
-           if(get_Linear_Actuator_B_LS_State()==0x01 || get_Linear_Actuator_B_LS_State()==0x03){
-               set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
-               actuator_state_B=ACTUATOR_STATE_IDLE;
-           }
-           if(actuator_B_cnt >= 20){
-               set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
-               actuator_state_B=ACTUATOR_STATE_IDLE;
-               actuator_B_cnt = 0;
-           }
-           break;
-       case ACTUATOR_STATE_MOVE_CCW:
-           actuator_B_cnt ++;
-           if(get_Linear_Actuator_B_LS_State()==0x02 || get_Linear_Actuator_B_LS_State()==0x03){
-               set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
-               actuator_state_B=ACTUATOR_STATE_IDLE;
-           }
-           if(actuator_B_cnt >= 20){
-               set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
-               actuator_state_B=ACTUATOR_STATE_IDLE;
-               actuator_B_cnt = 0;
-           }
-           break;
-       default: 
-           break;
-   }
+        case ACTUATOR_STATE_IDLE:
+            xTimerStop(detect_LS_TimerB, 0);
+            break;//wait the next command into.
+
+        case ACTUATOR_STATE_MOVE_CW:
+            actuator_B_cnt ++;
+            if(get_Linear_Actuator_B_LS_State()==0x01 || get_Linear_Actuator_B_LS_State()==0x03){
+                set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
+                actuator_state_B = ACTUATOR_STATE_IDLE;
+            }
+            if(actuator_B_cnt >= 20){
+                set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
+                actuator_state_B = ACTUATOR_STATE_IDLE;
+                actuator_B_cnt = 0;
+            }
+            break;
+
+        case ACTUATOR_STATE_MOVE_CCW:
+            actuator_B_cnt ++;
+            if(get_Linear_Actuator_B_LS_State()==0x02 || get_Linear_Actuator_B_LS_State()==0x03){
+                set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
+                actuator_state_B = ACTUATOR_STATE_IDLE;
+            }
+            if(actuator_B_cnt >= 20){
+                set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
+                actuator_state_B = ACTUATOR_STATE_IDLE;
+                actuator_B_cnt = 0;
+            }
+            break;
+
+        default:
+            break;
+    }
 }
 
-int get_Linear_Actuator_A_LS_State(void){
+int get_Linear_Actuator_A_LS_State(void)
+{
     static int actuator_A_LS_state;
     actuator_A_LS_state  = GPIO_ReadInputDataBit(LS_READ_PORT, LS_A_UPPER_PIN)? 0x01 : 0x00; /*upper limited*/
     actuator_A_LS_state |= GPIO_ReadInputDataBit(LS_READ_PORT, LS_A_LOWER_PIN)? 0x02 : 0x00; /*lower limited*/
@@ -187,11 +201,13 @@ int get_Linear_Actuator_A_LS_State(void){
      * 00 :  normal range.
      * 01 :  upper limited
      * 02 :  lower limited
-     * 03 :  both limited, but isn't impossible in actually.
-     */ 
+     * 03 :  both limited, but is impossible actually.
+     */
     return actuator_A_LS_state;
 }
-int get_Linear_Actuator_B_LS_State(void){
+
+int get_Linear_Actuator_B_LS_State(void)
+{
     static int actuator_B_LS_state;
     actuator_B_LS_state  = GPIO_ReadInputDataBit(LS_READ_PORT, LS_B_UPPER_PIN)? 0x01 : 0x00; /*upper limited*/
     actuator_B_LS_state |= GPIO_ReadInputDataBit(LS_READ_PORT, LS_B_LOWER_PIN)? 0x02 : 0x00; /*lower limited*/
@@ -199,8 +215,8 @@ int get_Linear_Actuator_B_LS_State(void){
      * 00 :  normal range.
      * 01 :  upper limited
      * 02 :  lower limited
-     * 03 :  both limited, but isn't impossible in actually.
-     */ 
+     * 03 :  both limited, but is impossible actually.
+     */
     return actuator_B_LS_state;
 }
 
--- epw_lib/linear_actuator.h
+++ epw_lib/linear_actuator.h
@@ -12,7 +12,7 @@
 #include "stm32f4xx_syscfg.h"
 
 
-#define ACTU_CONTROL_PORT             GPIOE
+#define ACTU_CONTROL_PORT           GPIOE
 
 #define ACTU_A_IN1_PIN              GPIO_Pin_0
 #define ACTU_A_IN2_PIN              GPIO_Pin_1
@@ -23,21 +23,21 @@
 /*
  *Limit Switch
  */
-#define LS_READ_PORT              GPIOD
+#define LS_READ_PORT                GPIOD
 #define LS_A_UPPER_PIN              GPIO_Pin_3
 #define LS_A_LOWER_PIN              GPIO_Pin_4
 #define LS_B_UPPER_PIN              GPIO_Pin_7
 #define LS_B_LOWER_PIN              GPIO_Pin_6
 
 enum{
-  LINEAR_ACTU_STOP,
-  LINEAR_ACTU_CW,
-  LINEAR_ACTU_CCW
+    LINEAR_ACTU_STOP,
+    LINEAR_ACTU_CW,
+    LINEAR_ACTU_CCW
 };
 
 struct limit_switch_info{
-  uint8_t actuatorA_LS_state;
-  uint8_t actuatorB_LS_state;
+    uint8_t actuatorA_LS_state;
+    uint8_t actuatorB_LS_state;
 };
 
 static void init_LS();
