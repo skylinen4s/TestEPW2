a124ea743c3e4e88a99b123c8060868e6b0f9fd1
skylinen4s <skyline_n4s@hotmail.com>
2016-06-20 19:23:28 +0800

    prepare math func. for Fuzzy Neural

--- epw_lib/clib.c
+++ epw_lib/clib.c
@@ -7,3 +7,38 @@ int math_round(float num){
 int math_abs(float num){
 	return (num > 0)? num : -(num);
 }
+
+float f_sign(float num){
+	return (num > 0.0f)? 1.0f : -1.0f;
+}
+
+float f_abs(float num){
+	return (num > 0.0f)? num : -(num);
+}
+
+float exponential(float ld){
+	float result = 1.0;
+	float term = ld;
+	int diaminator = 2;
+	int count = 0;
+	int test = 0;
+
+	while(count < 10){
+		result = result + term;
+		term = term * ld;
+		term = term / (float)diaminator;
+		(int)diaminator++;
+		count++;
+		test = math_abs(term*1000);
+		if(test < 10) break;
+	}
+	return result;
+}
+
+float math_pow(float num, int times){
+	int i = 0;
+	float tmp = 1.0f;
+	for(i = 0; i < times; ++i) tmp = num * tmp;
+
+	return tmp;
+}
--- epw_lib/clib.h
+++ epw_lib/clib.h
@@ -5,5 +5,9 @@
 
 int math_round(float num);
 int math_abs(float num);
+float math_pow(float num, int times);
+float f_sign(float num);
+float f_abs(float num);
+float exponential(float ld);
 
 #endif
